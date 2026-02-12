f16104f43ad42b0825efcf004292f0537d75759a
Zahim Ahmed Khan <zahim1632@gmail.com>
2026-02-13 02:27:46 +0500

    update import of file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import{
   Navigate,
 } from 'react-router-dom'
 import Login from './pages/Auth/Login'
-import SignUp from './pages/Auth/Signup'
+import SignUp from './pages/Auth/SignUp'
 import Home from './pages/Dashboard/Home'
 import Income from './pages/Dashboard/Income'
 import Expense from './pages/Dashboard/Expense'
